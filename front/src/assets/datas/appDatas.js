dde7fe17dd191dada689d595cc157a61462300bd
Florian Merrien <fmerrien.dev@gmail.com>
2020-07-20 11:54:51 +0200

    Application component for desktop done

---
+++ front/src/assets/datas/appDatas.js
@@ -0,0 +1,43 @@
+import background from '../images/background.svg';
+import app1 from '../images/app1.svg';
+import googlePlay from '../images/googlePlay.svg';
+import appStore from '../images/appStore.svg';
+
+export default [
+    {
+        index : 0,
+        title : "Des idées selon vos envies",
+        subTitle : "TROUVEZ LES COCKTAILS REALISABLES AVEC LES INGREDIENTS DISPONIBLES CHEZ VOUS",
+        text : "Nous nous sommes tous déjà retrouvés à la maison avec l’envie de prendre un verre sans la motivation pour faire des courses. Avec la fonctionnalité « Frigo », il suffit de rentrer les ingrédients disponibles chez vous et l’application vous trouvera tous les cocktails réalisables sans sortir de chez vous ! Pour aller plus loin, l’app vous recommandera aussi les ingrédients qui vous manquent pour débloquer toujours plus de recettes.",
+        img : app1,
+        subImg : [googlePlay, appStore],
+        background
+    },
+    {
+        index : 1,
+        title : "Des recommandations personnalisées",
+        subTitle : "TROUVEZ LES COCKTAILS REALISABLES AVEC LES INGREDIENTS DISPONIBLES CHEZ VOUS",
+        text : "Nous nous sommes tous déjà retrouvés à la maison avec l’envie de prendre un verre sans la motivation pour faire des courses. Avec la fonctionnalité « Frigo », il suffit de rentrer les ingrédients disponibles chez vous et l’application vous trouvera tous les cocktails réalisables sans sortir de chez vous ! Pour aller plus loin, l’app vous recommandera aussi les ingrédients qui vous manquent pour débloquer toujours plus de recettes.",
+        img : app1,
+        subImg : [googlePlay, appStore],
+        background
+    },
+    {
+        index : 2,
+        title : "+ 300 recettes exclusives",
+        subTitle : "TROUVEZ LES COCKTAILS REALISABLES AVEC LES INGREDIENTS DISPONIBLES CHEZ VOUS",
+        text : "Nous nous sommes tous déjà retrouvés à la maison avec l’envie de prendre un verre sans la motivation pour faire des courses. Avec la fonctionnalité « Frigo », il suffit de rentrer les ingrédients disponibles chez vous et l’application vous trouvera tous les cocktails réalisables sans sortir de chez vous ! Pour aller plus loin, l’app vous recommandera aussi les ingrédients qui vous manquent pour débloquer toujours plus de recettes.",
+        img : app1,
+        subImg : [googlePlay, appStore],
+        background
+    },
+    {
+        index : 3,
+        title : "Des recettes pas à pas",
+        subTitle : "TROUVEZ LES COCKTAILS REALISABLES AVEC LES INGREDIENTS DISPONIBLES CHEZ VOUS",
+        text : "Nous nous sommes tous déjà retrouvés à la maison avec l’envie de prendre un verre sans la motivation pour faire des courses. Avec la fonctionnalité « Frigo », il suffit de rentrer les ingrédients disponibles chez vous et l’application vous trouvera tous les cocktails réalisables sans sortir de chez vous ! Pour aller plus loin, l’app vous recommandera aussi les ingrédients qui vous manquent pour débloquer toujours plus de recettes.",
+        img : app1,
+        subImg : [googlePlay, appStore],
+        background
+    }   
+]
--- front/src/components/Home/components/Application/Slider/index.js
+++ front/src/components/Home/components/Application/Slider/index.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./slider.scss";
 
+// Datas
+import appDatas from '../../../../../assets/datas/appDatas';
+import {ReactComponent as Line } from '../../../../../assets/images/Line.svg';
+import btnLeft from '../../../../../assets/images/btnLeft.svg';
+import btnRight from '../../../../../assets/images/btnRight.svg';
+import { ReactComponent as SecondLine } from '../../../../../assets/logos/line.svg';
+
 const Slider = () => {
+
+    const maxIndex = appDatas.length - 1;
+    const [index, setIndex] = useState(0);
+
+    const handleClickLeft = () => {
+        if (index === 0) {
+            setIndex(maxIndex)
+        } else {
+            setIndex(index - 1)
+        }
+    };
+
+    const handleClickRight = () => {
+        if (index === maxIndex) {
+            setIndex(0)
+        } else {
+            setIndex(index + 1)
+        }
+    };
+
     return (
         <div className="slider">
-            
+            <div className="slider-titles">
+                {appDatas.map((data) => (
+                    <h1 id={data.index === index ? "active" : ""}>{data.title}</h1>
+                ))}
+            </div>
+            <Line className="slider-line" alt="line.svg"/>
+            <div className="slider-slide">
+                <img src={appDatas[index].background} alt="background"/>
+                <img src={appDatas[index].img} id="absImg"alt="absolute"/>
+                <h1 id="absTitle">{appDatas[index].subTitle}</h1>
+                <p id="absText">{appDatas[index].text}</p>
+                <div id="absSubImg">
+                    {appDatas[index].subImg.map((img) => (
+                        <img src={img} alt="subImg"/>
+                    ))}
+                </div>
+                <img className="slider-btn left" src={btnLeft} alt="btn left" onClick={() => handleClickLeft()}/>
+                <img className="slider-btn right" src={btnRight} alt="btn right" onClick={() => handleClickRight()}/>
+            </div>
+            <SecondLine className="lineSvgBottom"/>
         </div>
     );
 };
